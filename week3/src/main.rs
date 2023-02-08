da8d3e358c5ad6bc5081413b35bf6c36397c03f0
YZWarren <yz735@duke.edu>
2023-02-08 13:18:07 -0500

    update dockerfile

---
+++ week3/src/main.rs
@@ -0,0 +1,27 @@
+/*An actix Microservice for Simple Calculator that has multiple routes:
+A. / that turns a hello world message
+B. /calculate/{string} that calculates the result of the string
+*/
+
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+//create a function that returns hello world
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World! Calculators are fun!")
+}
+
+//create a function that returns the result of the calculation
+#[get("/calculate/{input}")]
+async fn calculate(input: web::Path<String>) -> impl Responder {
+    let result = week3::calculate(input.to_string());
+    HttpResponse::Ok().body(result.to_string())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello).service(calculate))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
+}
